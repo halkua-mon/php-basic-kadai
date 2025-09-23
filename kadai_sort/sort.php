cc987686bd9c757172d670ddf9f18de29ad1ca82
halkua-mon <hakumon.ja@gmail.com>
2025-09-23 21:34:21 +0900

    Update sort.phpを編集

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -11,21 +11,27 @@
         <?php
         $arrays = [15, 4, 18, 23, 10 ];
 
-        if ($order = TRUE) {
-        sort($arrays);
-          echo '昇順にソートします。<br>';
-        foreach ($arrays as $array) {
-          echo "{$array} <br>";
+        function sort_2way($arrays,$order) {
+
+          if ($order) {
+            echo '昇順にソートします。<br>';
+            sort($arrays);
+            foreach ($arrays as $array) {
+            echo "{$array} <br>";
+            }
+          }
+
+          else {
+            echo '降順にソートします。<br>';
+            rsort($arrays);
+            foreach ($arrays as $array) {
+            echo "{$array} <br>";
+            }
           }
         }
 
-
-          arsort($arrays);
-          echo '降順にソートします。<br>';
-        foreach ($arrays as $array) {
-          echo "{$array} <br>";
-          }
-
+          sort_2way($arrays, true);
+          sort_2way($arrays, false);
         ?>
     </p>
 </body>
